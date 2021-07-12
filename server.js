e88ba03a5d2c686592d951befc834fa1663f2bc3
Tanya Y <brulek7@gmail.com>
2021-07-12 19:51:36 -0400

    connected env.MONGODB_URI

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/btracker", {
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/btracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
